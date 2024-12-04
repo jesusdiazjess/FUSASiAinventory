2838eebbda209e82194adac8477f13541b89ac9f
Jesus Diaz <jesusdiazjess@gmail.com>
2024-12-04 21:24:02 +0800

    Add files via upload

---
+++ FUSASIA/includes/footer.php
@@ -0,0 +1,10 @@
+</div> <!--CONTAINER-->
+	<!--EXTERNAL FUSASIA FILE INPUT-->
+	<script src="assests/plugins/fileinput/js/plugins/canvas-to-blob.min.js" type="text/javascript"></script>	
+	<script src="assests/plugins/fileinput/js/plugins/sortable.min.js" type="text/javascript"></script>	
+	<script src="assests/plugins/fileinput/js/plugins/purify.min.js" type="text/javascript"></script>
+	<script src="assests/plugins/fileinput/js/fileinput.min.js"></script>	
+	<!-- FUSASIS DataTables -->
+	<script src="assests/plugins/datatables/jquery.dataTables.min.js"></script>
+</body>
+</html>
